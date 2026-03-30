154ea099b3754e06a8fa60ee32f427ca7785e021
Dawson Timmons <DawsonTimmons@gmail.com>
2026-03-29 21:15:50 -0600

    deployment changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# Use this on Render free tier since pre-deploy commands require a paid instance
 bundle exec rails db:migrate
